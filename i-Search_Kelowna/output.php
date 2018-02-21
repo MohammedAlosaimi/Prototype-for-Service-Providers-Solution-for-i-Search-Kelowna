c27ed9d3c0323c38eaa3695536e0e687d53453cb
escherban <scherban.eric@gmail.com>
2018-02-21 13:28:34 -0800

    PHP
    Ran some queries

--- i-Search_Kelowna/output.php
+++ i-Search_Kelowna/output.php
@@ -1,17 +1,31 @@
+
 <?php
 if (isset($_POST['op1'])){
    $in = "";
+   $size = 0;
    $mysqli = mysqli_connect("localhost", "root", "", "i-searchkelownadb");
-   $sql = "SELECT "COL' '10"
+   $sql = "SELECT DISTINCT(COL10)
+           FROM tableone";
+   $sql2 = "SELECT DISTINCT(COL12)
            FROM tableone";
    $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+   $result2 = mysqli_query($mysqli, $sql2) or die(mysqli_error($mysqli));
    $rArray = array();
-   $counter = 0;
-   while ($info = mysqli_fetch_array($result)){
-       $in = stripslashes($info["COL' '10"]);    
-                array_push($rArray, $in);
-            }
-            echo $counter;
+   $rArray2 = array();
+    while ($info = mysqli_fetch_array($result)){       
+       $in = stripslashes($info["COL10"]);
+       array_push($rArray, $in);
+    }
+    while ($info = mysqli_fetch_array($result2)){       
+       $in = stripslashes($info["COL12"]);
+       array_push($rArray2, $in);
+    }
+    $size = sizeof($rArray);
+    for ($i = 1; $i < $size; $i++) {
+    echo $rArray[$i], "<br>";
+    $temp = $rArray2[$i];
+    echo "Description:", $temp, "<br>";
+    }
 }
 
 if (isset($_POST['op2'])){
