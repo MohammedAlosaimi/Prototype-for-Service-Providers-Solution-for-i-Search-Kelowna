105ef427281a3b8cd2842b75e40022598bd79ebd
Gurpyar <gurpyarbrar44@gmail.com>
2018-03-18 00:56:50 -0700

    changes

--- i-Search_Kelowna/output.php
+++ i-Search_Kelowna/output.php
@@ -1,181 +1,196 @@
 
 <?php
-   //ID 1 to 6 are general search subjects. ID 7 is refine search. ID 8 is to get contact information
-   //make an array of currently selected options, numbers which corespond to them, nested for echo where array contains $j 
- //$data = $_POST['data']; 
- //echo $data;
- 
-$id = $_GET['id'];
-   $keywords;
-   $ageDemo;
-   $rent;
-   $buildingType;
-   $gender;
-   $alcohol;
-   $disAcc;
-   $pets;
-   $mysqli = mysqli_connect("localhost", "root", "", "isearch");
-   $allArray = array(array());
-   $allSearch = "SELECT DISTINCT title, description, layer_title, 
-                                 Building_Type, Services_Provided, Other_Services, 
-                                 Building_Accommodated_Individuals_with_Disabilities, 
-                                 Monthly_Cost_Calculation, Building_is_Pet_Friendly, 
-                                 Accommodations_For_Smoking, Residents_are_required_to_abstain_from_alcohol_and_drugs,
-                                 Level_of_Drugs_and_Alcohol_Tolerence, Demographic_Served,
-                                 Search_Keywords, id, Gender_Served, Primary_Target_Resident, Other_Services
-                 FROM isearch";
-   $allResult = mysqli_query($mysqli, $allSearch) or die(mysqli_error($mysqli));
-   $inner = 0;
-   $outer = 0;
-    while ($info = mysqli_fetch_array($allResult)){  
+//ID 1 to 6 are general search subjects. ID 7 is refine search. ID 8 is to get contact information
+/*FIELDS TO USE: title, description, layer_title, Building_Type, Services_Provided, Other_Services, 
+Building_Accommodated_Individuals_with_Disabilities, Monthly_Cost_Calculation, Building_is_Pet_Friendly, 
+Accommodations_For_Smoking, Residents_are_required_to_abstain_from_alcohol_and_drugs,
+Level_of_Drugs_and_Alcohol_Tolerence, Demographic_Served,Search_Keywords, id, Gender_Served, 
+Primary_Target_Resident, Other_Services*/
+echo "here";
+$test = $_POST['test'];
+
+//$id = $_GET['id'];    
+$keywords;
+$ageDemo;
+$rent;
+$buildingType;
+$gender;
+$alcohol;
+$disAcc;
+$pets;
+$mysqli = mysqli_connect("localhost", "root", "", "isearch");
+   
+if ($id == 1){ 
+    //title, description
+    $arrayOne = array(array());
+    $sqlOne = "SELECT DISTINCT title, description
+               FROM isearch";
+    $resultOne = mysqli_query($mysqli, $sqlOne) or die(mysqli_error($mysqli));
+    $inner = 0;
+    $outer = 0;
+    while($info = mysqli_fetch_array($resultOne)){
         $in1 = stripslashes($info["title"]);
-            $allArray[$outer][$inner] = $in1;
+        $arrayOne[$outer][$inner] = $in1;
         $in2 = stripslashes($info["description"]);
-            $allArray[$outer][$inner + 1] = $in2;
-        $in3 = stripslashes($info["layer_title"]);
-            $allArray[$outer][$inner + 2] = $in3;
-        $in4 = stripslashes($info["Building_Type"]);
-            $allArray[$outer][$inner + 3] = $in4;
-        $in5 = stripslashes($info["Services_Provided"]);
-            $allArray[$outer][$inner + 4] = $in5;
-        $in6 = stripslashes($info["Other_Services"]);
-            $allArray[$outer][$inner + 5] = $in6;
-        $in7 = stripslashes($info["Building_Accommodated_Individuals_with_Disabilities"]);
-            $allArray[$outer][$inner + 6] = $in7;
-        $in8 = stripslashes($info["Monthly_Cost_Calculation"]);
-            $allArray[$outer][$inner + 7] = $in8;
-        $in9 = stripslashes($info["Building_is_Pet_Friendly"]);
-<<<<<<< HEAD
-            $allArray[$outer][$inner + 8] = $in9;
-        $in10 = stripslashes($info["Accommodations_For_Smoking"]);
-            $allArray[$outer][$inner + 9] = $in10;
-        $in11 = stripslashes($info["Residents_are_required_to_abstain_from_alcohol_and_drugs"]);
-            $allArray[$outer][$inner + 10] = $in11;
-        $in12 = stripslashes($info["Level_of_Drugs_and_Alcohol_Tolerence"]);
-            $allArray[$outer][$inner + 11] = $in12;
-        $in13 = stripslashes($info["Demographic_Served"]); 
-            $allArray[$outer][$inner + 12] = $in13;
-        $in14 = stripslashes($info["Search_Keywords"]);
-            $allArray[$outer][$inner + 13] = $in14;
-        $in15 = stripslashes($info["id"]);
-            $allArray[$outer][$inner + 14] = $in15;
-        $in16 = stripslashes($info["Gender_Served"]);
-            $allArray[$outer][$inner + 15] = $in16;
-        $in17 = stripslashes($info["Primary_Target_Resident"]);
-            $allArray[$outer][$inner + 16] = $in17;
-        $in18 = stripslashes($info["Other_Services"]);
-            $allArray[$outer][$inner + 17] = $in18;
-        $outer ++;
-    } 
-    $numOfRows = count($allArray);
-    $numOfCols = count($allArray[0]);   
-if ($id == 1){     
-   for($i = 0; $i < $numOfRows; $i ++){
-        echo $allArray[$i][0] . "<br>"; 
-        echo "<br>";
-    } 
-=======
+        $arrayOne[$outer][$inner + 1] = $in2;
+        $outer++;
     }
-if ($id == 1){  
-   $sql = "SELECT DISTINCT title, description, id
-           FROM isearch";
-   $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
-   $rArray = array();
-    while ($info = mysqli_fetch_array($result)){  
-        $temp = "";
-        $in1 = stripslashes($info["title"]);
-        $in2 = stripslashes($info["description"]);
-        if($in2 == NULL){
-            $in2 = "";
+    $numOfRows = count($arrayOne);
+    $numOfCols = count($arrayOne[0]);
+    for($i = 0; $i < $numOfRows; $i++){
+        echo $arrayOne[$i][0] . "<br>";
+        if($arrayOne[$i][1] != NULL){       
+            echo $arrayOne[$i][1] . "<br>";
         }
-        $temp = "<li  class='q1'>" . "<b>" . $in1 . "</b>" . " | ". "<a onclick = 'contactInfo.js' href = 'javascript:void(0);' >Contact Info</a>" . "</li>";
-        array_push($rArray, $temp);
+        echo "<br>";
     }
-    $size = sizeof($rArray);
-    for ($i = 0; $i < $size; $i++){       
-        echo $rArray[$i];
-    }
->>>>>>> e7a283937347fd5be963e97fa89f4457e0929451
 }
 if ($id == 2){
-    for($i = 0; $i < $numOfRows; $i ++){
-        echo $allArray[$i][0] . "<br>";
-        echo $allArray[$i][3] . "<br>";
-        echo $allArray[$i][4] . "<br>";
-        echo "<br>";
+    //title, layer title, building type
+    $arrayTwo = array(array());
+    $sqlTwo = "SELECT DISTINCT title, layer_title, building_type 
+               FROM isearch";
+    $resultTwo = mysqli_query($mysqli, $sqlTwo) or die(mysqli_error($mysqli));
+    $inner = 0;
+    $outer = 0;
+    while($info = mysqli_fetch_array($resultTwo)){
+        $in1 = stripslashes($info["title"]);
+        $arrayTwo[$outer][$inner] = $in1;
+        $in2 = stripslashes($info["layer_title"]);
+        $arrayTwo[$outer][$inner + 1] = "Service type: " . $in2;
+        $in3 = stripslashes($info["building_type"]);
+        $arrayTwo[$outer][$inner + 2] = "Building type: " . $in3;
+        $outer++;
+    }
+    $numOfRows = count($arrayTwo);
+    $numOfCols = count($arrayTwo[0]);
+    for($i = 0; $i < $numOfRows; $i++){
+        if($arrayTwo[$i][1] != NULL && $arrayTwo[$i][2] != NULL){ 
+            echo $arrayTwo[$i][0] . "<br>";       
+            echo $arrayTwo[$i][1] . "<br>";
+            echo $arrayTwo[$i][2] . "<br>";
+            echo "<br>";
+       }
     }
 }
 if ($id == 3){
-<<<<<<< HEAD
-    for($i = 0; $i < $numOfRows; $i ++){
-        echo $allArray[$i][0] . "<br>";
-        echo $allArray[$i][9] . "<br>";
-        echo $allArray[$i][10] . "<br>";  
-        echo "<br>";
-=======
-    $sqlOp3 = "SELECT DISTINCT title, Accommodations_For_Smoking, Residents_are_required_to_abstain_from_alcohol_and_drugs, Level_of_Drugs_and_Alcohol_Tolerence  
-               FROM isearch";
-   $result3 = mysqli_query($mysqli, $sqlOp3) or die(mysqli_error($mysqli));
-   $rArray3 = array();
-    while ($info = mysqli_fetch_array($result3)){  
-        $temp = "";
+    //Title, Accommodations_For_Smoking, Residents_are_required_to_abstain_from_alcohol_and_drugs
+    $arrayThree = array(array());
+    $sqlThree = "SELECT DISTINCT title, Accommodations_For_Smoking, 
+                                 Residents_are_required_to_abstain_from_alcohol_and_drugs
+                 FROM isearch";
+    $resultThree = mysqli_query($mysqli, $sqlThree) or die(mysqli_error($mysqli));
+    $inner = 0;
+    $outer = 0;
+    while($info = mysqli_fetch_array($resultThree)){
         $in1 = stripslashes($info["title"]);
+        $arrayThree[$outer][$inner] = $in1;
         $in2 = stripslashes($info["Accommodations_For_Smoking"]);
-        $in3 = stripslashes($info["Residents_are_required_to_abstain_from_alcohol_and_drugs"]);
-        $in4 = stripslashes($info["Level_of_Drugs_and_Alcohol_Tolerence"]);
         if($in2 == NULL){
-           $in2 = "A) No smoking accomodations.";
+            $in2 = "No smoking accomodations at this location.";
         }
         else{
-           $in2 = "A) " . $in2;  
+            $in2 = "Smoking accommodations: " . $in2;
         }
-        if($in3 == "true"){
-            $in3 = "A) Yes.";}            
-        else if($in3 == "false"){
-            $in3 = "A) No.";}
-        else{
-            $in3 = "n/a";}
-        if($in4 == NULL){
-            $in4 = "n/a";
+        $arrayThree[$outer][$inner + 1] = $in2;
+        $in3 = stripslashes($info["Residents_are_required_to_abstain_from_alcohol_and_drugs"]);
+        if($in3 == "false"){
+            $in3 = "Residents who stay at this location ARE NOT required to abstain from alcohol and drugs.";
         }
-        $in2 = "<div id='qs'>"."Q) Are there accomodations for smoking? " . $in2 . "<br>";
-        $in3 = "Q) Are residents required to abstain from alcohol and drugs? " . $in3 . "<br";
-        $in4 = "Q) Level of drug and alcohol tolerence: ". $in4 . "<br></div>";
-       
-        
-        //This is to be changed.
-        
-        $temp = "<li class='q2'><b><a id ='firsta'>" . $in1 . "</a></b>" . " | ". "<a id='seconda' onclick = 'contactInfo.js' href = 'javascript:void(0);' >Contact Info</a>" . "</li>" . $in2 . $in3 . $in4 . "<br>";
-        
-        
-        
-        
-        array_push($rArray3, $temp);
->>>>>>> e7a283937347fd5be963e97fa89f4457e0929451
+        else if($in3 == "true"){
+            $in3 = "Residents who stay at this location ARE required to abstain from alcohol and drugs.";
+        }
+        $arrayThree[$outer][$inner + 2] = $in3;
+        $outer++;
+    }
+    $numOfRows = count($arrayThree);
+    $numOfCols = count($arrayThree[0]);
+    for($i = 0; $i < $numOfRows; $i++){
+        if($arrayThree[$i][1] != NULL || $arrayThree[$i][2] != NULL){ 
+            echo $arrayThree[$i][0] . "<br>";       
+            echo $arrayThree[$i][1] . "<br>";
+            echo $arrayThree[$i][2] . "<br>";
+            echo "<br>";
+       }
     }
 }
 if ($id == 4){
-    for($i = 0; $i < $numOfRows; $i ++){
-        echo $allArray[$i][0] . "<br>";
-        echo $allArray[$i][12] . "<br>";
-        echo $allArray[$i][15] . "<br>";
-        echo $allArray[$i][16] . "<br>";
-        //echo $allArray[$i][17] . "<br>";
-        echo "<br>";
-    }    
+    //title, Demographic_Served, Gender_Served, Primary_Target_Resident 
+    $arrayFour = array(array());
+    $sqlFour = "SELECT DISTINCT title, Demographic_Served, Gender_Served, Primary_Target_Resident
+                FROM isearch";
+    $resultFour = mysqli_query($mysqli, $sqlFour) or die(mysqli_error($mysqli));
+    $inner = 0;
+    $outer = 0;
+    while($info = mysqli_fetch_array($resultFour)){
+        $in1 = stripslashes($info["title"]);
+        $arrayFour[$outer][$inner] = $in1;
+        $in2 = stripslashes($info["Demographic_Served"]);
+        if($in2 == NULL){
+            $in2 = "No preference";
+        }
+        $arrayFour[$outer][$inner + 1] = "Age demographic served: " . $in2;
+        $in3 = stripslashes($info["Gender_Served"]);
+        if($in3 == NULL){
+            $in3 = "No preference";
+        }
+        $arrayFour[$outer][$inner + 2] = "Gender Served: " . $in3;
+        $in4 = stripslashes($info["Primary_Target_Resident"]);
+        if($in4 == NULL){
+            $in4 = "No preference";
+        }
+        $arrayFour[$outer][$inner + 3] = "Target resident: ". $in4;
+        $outer++;
+    }
+    $numOfRows = count($arrayFour);
+    $numOfCols = count($arrayFour[0]);
+    for($i = 0; $i < $numOfRows; $i++){
+            echo $arrayFour[$i][0] . "<br>";       
+            echo $arrayFour[$i][1] . "<br>";
+            echo $arrayFour[$i][2] . "<br>";
+            echo $arrayFour[$i][3] . "<br>";
+            echo "<br>";
+    }
 }
 if ($id == 5){
-    
+    //title, Services_Provided, Other_Services
+    $arrayFive = array(array());
+    $sqlFive = "SELECT DISTINCT title, Services_Provided, Other_Services               
+                FROM isearch";
+    $resultFive = mysqli_query($mysqli, $sqlFive) or die(mysqli_error($mysqli));
+    $inner = 0;
+    $outer = 0;
+    while($info = mysqli_fetch_array($resultFive)){
+        $in1 = stripslashes($info["title"]);
+        $arrayFive[$outer][$inner] = $in1;
+        $in2 = stripslashes($info["Services_Provided"]);
+        if($in2 != NULL){
+            $in2 = "Primary Services: " . $in2;
+        }
+        $arrayFive[$outer][$inner + 1] = $in2;
+        $in3 = stripslashes($info["Other_Services"]);
+        if($in3 != NULL){
+            $in3 = "Additional services: " . $in3;  
+        }
+        $arrayFive[$outer][$inner + 2] = $in3;
+        $outer++;
+    }
+    $numOfRows = count($arrayFive);
+    $numOfCols = count($arrayFive[0]);
+    for($i = 0; $i < $numOfRows; $i++){
+        if($arrayFive[$i][1] != NULL){
+            echo $arrayFive[$i][0] . "<br>";       
+            echo $arrayFive[$i][1] . "<br>";
+            echo $arrayFive[$i][2] . "<br>";
+            echo "<br>";
+        }
+    }
 }
 if ($id == 6){
     
 }
 if ($id == 7){
-    for($i = 0; $i < $numOfRows; $i ++){
-        for($j = 0; $j < $numOfCols; $j ++){
-            
-        }
-    }
+
 }
 if ($id == 8){    
     $ContactInfo = "SELECT title, contact_person, phone_number, email
